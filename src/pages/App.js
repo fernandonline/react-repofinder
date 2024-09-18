f5571e531a52d0efca548e943b6822a87ebc9527
Fernando S. <luizfernandosr@outlook.com.br>
2024-09-18 17:51:22 -0300

    primeiro commit

---
+++ src/pages/App.js
@@ -0,0 +1,16 @@
+import logoGit from '../assets/github-mark-white.svg'
+import {Container} from './style';
+
+function App() {
+  return (
+
+    <Container>
+      <img src={logoGit} width={72} height={72} alt='logo do github'/>
+    </Container>
+
+
+
+  );
+}
+
+export default App
